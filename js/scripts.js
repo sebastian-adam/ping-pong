cc24d319ffc684875bbc21c1a96aa62fa1371483
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-06-10 21:24:21 -0700

    refactor repetitive js into functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,23 @@
-// BACKEND
+////////// BACKEND //////////
+
+// Runs the game
 var gameArray = [];
-
-var gameForfeit = function(counter, total) {
-  for (var i = 0; i < total; i++) {
-    if ((counter % 3 === 0) && (counter % 5 === 0)) {
-      gameArray.push("ping-pong");
-      counter -= 1;
-    } else if (counter % 5 === 0) {
-      gameArray.push("pong");
-      counter -= 1;
-    } else if (counter % 3 === 0) {
-      gameArray.push("ping");
-      counter -= 1;
-    } else {
-      gameArray.push(counter);
-      counter -= 1;
-    }
-  }
-  return gameArray;
-}
-
 var gamePlay = function(counter, total, version) {
   var alert = "TIME'S UP!";
   for (var i = 0; i <= total; i++) {
-      if (counter === 0){
-      gameArray.push(alert.fontcolor("red"));
+      if ((counter === 0) && (version === 1)) {
+      gameArray.push(alert.fontcolor("red").fontsize("6px"));
       counter -= 1;
-    } else if ((counter % 3 === 0) && (counter % 5 === 0)) {
+    } else if ((counter % 3 === 0) && (counter % 5 === 0) && (counter !== 0)) {
       gameArray.push("ping-pong");
       counter -= 1;
-    } else if (counter % 5 === 0) {
+    } else if (counter % 5 === 0 && (counter !== 0)) {
       gameArray.push("pong");
       counter -= 1;
-    } else if (counter % 3 === 0) {
+    } else if (counter % 3 === 0 && (counter !== 0)) {
       gameArray.push("ping");
       counter -= 1;
-    } else {
+    } else if (counter !== 0) {
       gameArray.push(counter);
       counter -= 1;
     }
@@ -43,43 +25,50 @@ var gamePlay = function(counter, total, version) {
   return gameArray;
 }
 
-
-
-
-
-
-
-// FRONTEND
-$(function() {
-  $("#reveal-all").click(function(event) {
-    event.preventDefault();
-
-    // Resets ul field and array
-    $("ul#result-field").empty();
-    gameArray = [];
-
-    var userNumber = parseInt($("#user-number").val());
-    var result = gameForfeit(userNumber, userNumber);
-
-    gameArray.forEach(function(i) {
-      $("ul#result-field").append("<li>" + i + "</li>");
-    });
+// Chooses button to act on
+var buttonChoice;
+var buttonChoice = function(btn, val) {
+    $("button" + btn).click(function(event) {
+    $("#play").prop("disabled",true);
+    $(btn + "-div").show();
+    buttonChoice = 0;
+    buttonChoice += val;
+    return buttonChoice;
   });
-  $("#play").click(function(event) {
-    event.preventDefault();
+}
 
-    $("ul#result-field").empty();
+
+////////// FRONTEND //////////
+$(function() {
+  buttonChoice("#reveal-all", 0);
+  buttonChoice("#play", 1);
+  buttonChoice("#reset", 2);
+
+  $("button").on('click', function(event) {
+    event.preventDefault();
+    // Resets ul fields, array, and button tracker
+    $("ul#play-field").empty();
+    $("ul#reveal-all-field").empty();
     gameArray = [];
 
     var userNumber = parseInt($("#user-number").val());
-    var result = gamePlay(userNumber, userNumber);
+    var result = gamePlay(userNumber, userNumber, buttonChoice);
     var time = 1000;
 
-    gameArray.forEach(function(i) {
-      setTimeout(function() {
-        $("ul#result-field").append("<li>" + i + "</li>");
-      }, time);
-      time += 1000;
-    });
+    if (buttonChoice === 2) {
+      window.location.reload();
+    } else if (buttonChoice === 1) {
+      gameArray.forEach(function(i) {
+        window.setTimeout(function() {
+          $("ul#play-field").append("<li>" + i + "</li>");
+        }, time);
+        time += 1000;
+      });
+    } else {
+      gameArray.forEach(function(i) {
+        $("ul#reveal-all-field").append("<li>" + i + "</li>");
+        $("div#rules").show();
+      });
+    }
   });
 });
