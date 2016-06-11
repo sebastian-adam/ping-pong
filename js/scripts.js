73306cd7c4e6becb2ed4311f26e71c734aa79a7f
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-06-10 21:27:24 -0700

    add css using bootstrap framework

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,21 @@
 
 // Runs the game
 var gameArray = [];
-var gamePlay = function(counter, total, version) {
+
+
+var gamePlay = function(counter, total, version, x, y) {
   var alert = "TIME'S UP!";
   for (var i = 0; i <= total; i++) {
       if ((counter === 0) && (version === 1)) {
       gameArray.push(alert.fontcolor("red").fontsize("6px"));
       counter -= 1;
-    } else if ((counter % 3 === 0) && (counter % 5 === 0) && (counter !== 0)) {
+    } else if ((counter % x === 0) && (counter % y === 0) && (counter !== 0)) {
       gameArray.push("ping-pong");
       counter -= 1;
-    } else if (counter % 5 === 0 && (counter !== 0)) {
+    } else if (counter % y === 0 && (counter !== 0)) {
       gameArray.push("pong");
       counter -= 1;
-    } else if (counter % 3 === 0 && (counter !== 0)) {
+    } else if (counter % x === 0 && (counter !== 0)) {
       gameArray.push("ping");
       counter -= 1;
     } else if (counter !== 0) {
