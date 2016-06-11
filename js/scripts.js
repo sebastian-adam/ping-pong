504465e5621fd8a1b83b33fd24a9fb864c2b7235
Sebastian Adam Prokuski <Subconscious@Sebastians-iMac.local>
2016-06-10 18:03:28 -0700

    add functional js for advanced behaviors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // BACKEND
 var gameArray = [];
 
-var gamePlay = function(counter, total) {
+var gameForfeit = function(counter, total) {
   for (var i = 0; i < total; i++) {
     if ((counter % 3 === 0) && (counter % 5 === 0)) {
       gameArray.push("ping-pong");
@@ -20,9 +20,38 @@ var gamePlay = function(counter, total) {
   return gameArray;
 }
 
+var gamePlay = function(counter, total, version) {
+  var alert = "TIME'S UP!";
+  for (var i = 0; i <= total; i++) {
+      if (counter === 0){
+      gameArray.push(alert.fontcolor("red"));
+      counter -= 1;
+    } else if ((counter % 3 === 0) && (counter % 5 === 0)) {
+      gameArray.push("ping-pong");
+      counter -= 1;
+    } else if (counter % 5 === 0) {
+      gameArray.push("pong");
+      counter -= 1;
+    } else if (counter % 3 === 0) {
+      gameArray.push("ping");
+      counter -= 1;
+    } else {
+      gameArray.push(counter);
+      counter -= 1;
+    }
+  }
+  return gameArray;
+}
+
+
+
+
+
+
+
 // FRONTEND
 $(function() {
-  $("form#play").submit(function(event) {
+  $("#reveal-all").click(function(event) {
     event.preventDefault();
 
     // Resets ul field and array
@@ -30,10 +59,27 @@ $(function() {
     gameArray = [];
 
     var userNumber = parseInt($("#user-number").val());
-    var result = gamePlay(userNumber, userNumber);
+    var result = gameForfeit(userNumber, userNumber);
 
     gameArray.forEach(function(i) {
       $("ul#result-field").append("<li>" + i + "</li>");
+    });
+  });
+  $("#play").click(function(event) {
+    event.preventDefault();
+
+    $("ul#result-field").empty();
+    gameArray = [];
+
+    var userNumber = parseInt($("#user-number").val());
+    var result = gamePlay(userNumber, userNumber);
+    var time = 1000;
+
+    gameArray.forEach(function(i) {
+      setTimeout(function() {
+        $("ul#result-field").append("<li>" + i + "</li>");
+      }, time);
+      time += 1000;
     });
   });
 });
